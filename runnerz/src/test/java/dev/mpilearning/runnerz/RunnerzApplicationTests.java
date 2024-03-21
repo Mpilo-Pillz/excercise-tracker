e4ff52965768dc8ef69bbd83662b7bbde8a67637
mpilopillz <mpilo.dlamini@yahoo.com>
2024-03-21 22:42:45 +0200

    initializing sprigboot application

---
+++ runnerz/src/test/java/dev/mpilearning/runnerz/RunnerzApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mpilearning.runnerz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnerzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
