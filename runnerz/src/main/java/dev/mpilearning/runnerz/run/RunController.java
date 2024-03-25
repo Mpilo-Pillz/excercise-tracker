d874dc1e222dc74339bea30b30ef7ae562e3bcf6
Fikasentani <mpilo.dlamini@yahoo.com>
2024-03-25 10:58:43 +0200

    gettint runs

--- runnerz/src/main/java/dev/mpilearning/runnerz/run/RunController.java
+++ runnerz/src/main/java/dev/mpilearning/runnerz/run/RunController.java
@@ -3,10 +3,17 @@ package dev.mpilearning.runnerz.run;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class RunController {
-    @GetMapping("/hello")
-    String home() {
-        return "Hello Runner";
+//        @Autowired // Not recomended makes testing harder
+    private final RunRepository runRepository;
+    public RunController(RunRepository runRepository) {
+        this.runRepository = runRepository;
+    }
+    @GetMapping("/api/runs")
+    List<Run> findAll() {
+        return this.runRepository.findAll();
     }
 }
