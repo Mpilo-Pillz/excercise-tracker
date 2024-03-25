32686f5c3381e03ba887037010a274425cad7fb5
Fikasentani <mpilo.dlamini@yahoo.com>
2024-03-25 10:58:32 +0200

    creating run repo

---
+++ runnerz/src/main/java/dev/mpilearning/runnerz/run/RunRepository.java
@@ -0,0 +1,26 @@
+package dev.mpilearning.runnerz.run;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class RunRepository {
+//    @Autowired // Not recomended makes testing harder
+    private List<Run> runs = new ArrayList<>();
+
+    List<Run> findAll() {
+        return runs;
+    }
+
+    @PostConstruct
+    private void init() {
+      runs.add(new Run(1, "Monday Morning Run", LocalDateTime.now(), LocalDateTime.now().plus(30, ChronoUnit.MINUTES), 3, Location.INDOOR));
+      runs.add(new Run(2, "Wednesday Evening Run", LocalDateTime.now(), LocalDateTime.now().plus(60, ChronoUnit.MINUTES), 6, Location.INDOOR));
+    }
+}
