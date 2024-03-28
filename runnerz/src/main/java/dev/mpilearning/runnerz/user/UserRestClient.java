ff9e957a6f75265eebd12bf75c5edf8b30bb396d
mpilopillz <mpilo.dlamini@yahoo.com>
2024-03-28 23:01:35 +0200

    using rest client

---
+++ runnerz/src/main/java/dev/mpilearning/runnerz/user/UserRestClient.java
@@ -0,0 +1,35 @@
+package dev.mpilearning.runnerz.user;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+
+@Component
+public class UserRestClient {
+    private final RestClient restClient;
+
+    public UserRestClient(RestClient.Builder builder) {
+        JdkClientHttpRequestFactory jdkClientHttpRequestFactory = new JdkClientHttpRequestFactory();
+        jdkClientHttpRequestFactory.setReadTimeout(6000);
+        this.restClient = builder.baseUrl("https://jsonplaceholder.typicode.com/")
+                .requestFactory(jdkClientHttpRequestFactory)
+                .build();
+    }
+
+    public List<User> findAll() {
+        return restClient.get()
+                .uri("/users")
+                .retrieve()
+                .body(new ParameterizedTypeReference<>() {});
+    }
+
+    public User fincById(Integer id) {
+        return restClient.get()
+                .uri("/users/{id}", id)
+                .retrieve()
+                .body(User.class);
+    }
+}
