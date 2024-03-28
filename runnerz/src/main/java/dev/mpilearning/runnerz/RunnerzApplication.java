ab7825b540258e6838d13b03953c1de3b707226b
mpilopillz <mpilo.dlamini@yahoo.com>
2024-03-28 23:03:50 +0200

    fetching users when appp boots up

--- runnerz/src/main/java/dev/mpilearning/runnerz/RunnerzApplication.java
+++ runnerz/src/main/java/dev/mpilearning/runnerz/RunnerzApplication.java
@@ -1,18 +1,19 @@
 package dev.mpilearning.runnerz;
 
-import dev.mpilearning.runnerz.run.Location;
-import dev.mpilearning.runnerz.run.Run;
-import dev.mpilearning.runnerz.run.RunRepository;
+import dev.mpilearning.runnerz.user.User;
+import dev.mpilearning.runnerz.user.UserHttpClient;
+import dev.mpilearning.runnerz.user.UserRestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 @SpringBootApplication
 public class RunnerzApplication {
@@ -22,6 +23,25 @@ public class RunnerzApplication {
 		SpringApplication.run(RunnerzApplication.class, args);
 		log.info("Exercise - Application Started Successfully");
 	}
+
+	@Bean
+	UserHttpClient userHttpClient() {
+		RestClient restClient = RestClient.create("https://jsonplaceholder.typicode.com/");
+		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
+		return factory.createClient(UserHttpClient.class);
+	}
+
+//		@Bean
+//		CommandLineRunner runner(userHttpClient client) {
+//		CommandLineRunner runner(UserRestClient client) {
+//		return args -> {
+//			List<User> users = client.findAll();
+//			System.out.println(users);
+//
+//			User user = client.fincById(2);
+//			System.out.println(user);
+//		};
+//	}
 
 //	@Bean
 //	CommandLineRunner runner(RunRepository runRepository) {
